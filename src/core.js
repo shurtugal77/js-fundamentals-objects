d9b1a8d3da244d9ebc65268dcd2c589a20284cd8
Robert Hohenester <robert.hohenester@googlemail.com>
2024-03-02 17:19:19 +0100

    its even prettier now

--- src/core.js
+++ src/core.js
@@ -2,7 +2,7 @@
 const person = { age: 32, size: 'Small' }
 
 // 1. Give the person object a name property with the value Matt
-person.name = "Matt"
+person.name = 'Matt'
 console.log(person)
 
 // 2. Remove the size property from the person
@@ -14,11 +14,11 @@ person.age += 11
 console.log(person)
 
 // 4. Add an instruments property to the person, initialised as an empty array
-person.instruments =  []
+person.instruments = []
 console.log(person)
 
 // 5. Add the following instruments to the persons instruments array: Guitar, Piano, Vocals
-person.instruments.push("Guitar", "Piano", "Vocals")
+person.instruments.push('Guitar', 'Piano', 'Vocals')
 console.log(person)
 console.log(person.instruments)
 
@@ -28,13 +28,12 @@ const mainInstrument = person.instruments[2]
 console.log(mainInstrument)
 
 // 7. Add a profession property to the person, which is an object
-const profession = {
-}
+const profession = {}
 person.profession = profession
 console.log(person)
 
 // 8. Add a name property to the profession object with the value Musician
-profession.name = "Musician"
+profession.name = 'Musician'
 console.log(person)
 
 // 9. Add a friends property to the person, which is an empty array
@@ -45,12 +44,12 @@ console.log(person)
 // Friend one: name - Chris, age - 46
 // Friend two: name - Dom, age - 43
 const friend1 = {
-  name: "Chris",
+  name: 'Chris',
   age: 46
 }
 
 const friend2 = {
-  name: "Dom",
+  name: 'Dom',
   age: 43
 }
 
