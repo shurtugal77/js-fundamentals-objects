ca40495436ea137d2f9e8a1f400320ad283d3309
Robert Hohenester <robert.hohenester@googlemail.com>
2024-03-02 17:37:52 +0100

    extension passed

--- src/extension.js
+++ src/extension.js
@@ -15,14 +15,23 @@ const book = {
   dimensions: '10x12x2'
 }
 
+delete book.isbn.asin
+delete book.dimensions
+book.pages = 464
+book.category = 'Programming'
+
 /* eslint-disable no-unused-vars */
 const isbn13 = '978-0132350884'
+book.isbn.isbn13 = isbn13
+console.log(book)
 
 // 1. Set this to the book name - using the book object
-const name = ''
+const name = book.name
+console.log(name)
 
 // 2. Set this to the isbn 10 value - using the book object
-const isbn10 = ''
+const isbn10 = book.isbn.isbn10
+console.log(isbn10)
 
 // Do not modify this basket object directly
 const basket = {
@@ -40,12 +49,21 @@ const basket = {
   ],
   voucherCodes: ['AA-AA-A', 'BB-BB-B']
 }
+basket.items[0].price = 2
+
+basket.items.push({
+  name: 'Oranges',
+  price: 0.75,
+  quantity: 4
+})
 
 // 3. Set this variable to the length of the baskets voucher codes array - using the basket object
-const numberOfVoucherCodes = null
+const numberOfVoucherCodes = basket.voucherCodes.length
+console.log(numberOfVoucherCodes)
 
 // 4. Set this variable to the first element in of the baskets voucher codes array - using the basket object
-const firstVoucherCode = null
+const firstVoucherCode = basket.voucherCodes[0]
+console.log(firstVoucherCode)
 
 // Do not edit this exported object
 module.exports = {
